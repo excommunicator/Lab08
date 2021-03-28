65a1fefe3c0a905d4abcca0e578ef80cd0acf24d
excommunicator <hallchris0001@gmail.com>
2021-03-28 18:00:32 -0400

    hello.py

---
+++ hello.py
@@ -0,0 +1,133 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello World, my name is Chris and I love Python!")
+Hello World, my name is Chris and I love Python!
+>>> userName = input ("Hello! Please enter you name!")
+
+print ("Hello", userName, "nice to meet you")
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> userName = input ("Hello! Please enter you name!");
+
+print ("Hello", userName, "nice to meet you");
+SyntaxError: multiple statements found while compiling a single statement
+>>> userName = input ("Hello! Please enter you name!")
+
+print ("Hello", userName, "nice to meet you")
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> userName = input ("Hello! Please enter you name!");
+Hello! Please enter you name!Chris Hall
+>>> print ("Hello", userName, "nice to meet you")
+Hello Chris Hall nice to meet you
+>>> 
+>>> 
+>>> userName = input ("Hello! Please enter you name!");
+Hello! Please enter you name!Chris
+>>> userName = input ("Hello! Please enter you name!");
+Hello! Please enter you name!Chris 
+>>> userName = input ("Hello! Please enter you name!");\
+	   print ("Hello", userName, "nice to meet you");
+Hello! Please enter you name!Chris
+Hello Chris nice to meet you
+>>> 
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+
+>>> 
+>>> 
+
+
+>>> 
+
+>>> 
+
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> cls
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    cls
+NameError: name 'cls' is not defined
+>>> 
+>>> 
+
+
+>>> 
+>>> 
+
+
+>>> 
+>>> 
+
+
+
+>>> 
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+
+>>> 
+>>> 
+
+>>> 
+
+
+>>> 
+>>> 
+
+>>> 
+
+>>> 
+
+
+>>> 
+>>> 
+
+
+>>> 
+
+>>> 
+
+>>> 
+
+>>> 
+
+
+>>> 
+>>> 
+
+
+>>> 
+>>> 
+>>> 
+>>> 
---
+++ username.py
@@ -0,0 +1,6 @@
+userName = input("Hello! Please enter your facebook username! ")
+userPassword = input("Hello! Please enter your facebook password! ")
+if(userName.lower() == "chris" and userPassword == "hello"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
